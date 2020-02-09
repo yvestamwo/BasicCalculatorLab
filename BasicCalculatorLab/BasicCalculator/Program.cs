e1ff6825311a4de750608d8bb38ffddfa885c4b2
yvestamwo <59981914+yvestamwo@users.noreply.github.com>
2020-02-09 00:41:19 -0800

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -8,9 +8,17 @@ namespace BasicCalculator
         {
             Console.WriteLine("*** BASIC CALCULATOR ***");
 
-            Console.WriteLine("Enter the first number");
+            Console.WriteLine("Enter the temperature in fahrenheit");
+            int temperature;
+            temperature = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter the wind speed");
+            int windspeed;
+            windspeed = int.Parse(Console.ReadLine());
+            double windchill = 35.74 + 0.6215 * temperature - 35.75 * windspeed + 0.16 + 0.4275 * temperature * windspeed;
+            Console.WriteLine("the wind chill is" + windchill);
+        }
 
-            // int.Parse will take a string data type and convert it to an int data type
+  /*          // int.Parse will take a string data type and convert it to an int data type
             int firstNumber = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter the second number");
@@ -19,6 +27,7 @@ namespace BasicCalculator
             int sum = firstNumber + secondNumber;
 
             Console.WriteLine("The answer is " + sum);
-        }
+  */
+        
     }
 }
