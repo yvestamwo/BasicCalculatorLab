a8ad922f1dc940438422855bac50549f48f7d7ec
yvestamwo <59981914+yvestamwo@users.noreply.github.com>
2020-02-15 21:48:35 -0800

    Update Program.cs

--- BasicCalculatorLab/BasicCalculator/Program.cs
+++ BasicCalculatorLab/BasicCalculator/Program.cs
@@ -1,24 +1,67 @@
 ﻿using System;
+using System.IO;
 
-namespace BasicCalculator
+namespace consoleApp1
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("*** BASIC CALCULATOR ***");
+        { 
+            string filename;
 
-            Console.WriteLine("Enter the first number");
 
-            // int.Parse will take a string data type and convert it to an int data type
-            int firstNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter filename");
+            filename = Console.ReadLine();
+            if (File.Exists(filename))
+            {
+                Console.WriteLine("File exists");
+                Console.WriteLine("enter threshold");
+                
 
-            Console.WriteLine("Enter the second number");
-            int secondNumber = int.Parse(Console.ReadLine());
 
-            int sum = firstNumber + secondNumber;
-
-            Console.WriteLine("The answer is " + sum);
+                string threshold = Console.ReadLine();
+                
+                int sumTemps = 0;
+                int numAbove = 0;
+                int numBelow = 0;
+                int temcount = 0;
+                using (StreamReader sr = File.OpenText(filename))
+                {
+                    string line = sr.ReadLine();
+                    int temp;
+                    while (line != null)
+                    {
+                        temp = int.Parse(line);
+                        sumTemps += 1;
+                        bool condition = (temp >= 20);
+                        if (condition)
+                        {
+                            numAbove += 1;
+                        }
+                        else
+                        {
+                            numBelow += 1;
+                        }
+                        line = sr.ReadLine();
+                        Console.WriteLine("Temps above =" + numAbove);
+                        Console.WriteLine("temps below =" + numBelow);
+                        int average = sumTemps / temcount;
+                        Console.WriteLine("Average temp =" + average);
+                        using (StreamWriter sw = new StreamWriter("output.txt"))
+                        {
+                            sw.WriteLine(System.DateTime.Now.ToString());
+                            sw.WriteLine("Temperature above =" + numAbove);
+                            sw.WriteLine("Temps below =" + numBelow);
+                            sw.WriteLine("Average temp = " + average);
+                        }
+                    }
+                    
+                    {
+                        Console.WriteLine("File does not exist");
+                    }
+                }
+            }
+            }
         }
     }
-}
+
